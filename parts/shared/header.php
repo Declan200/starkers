bed64cf8365ff44f5be932d47656619877cdf088
Declan200 <declan2317@hotmail.com>
2020-10-26 18:53:24 +1100

    added website logo and changed theme image

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -2,4 +2,9 @@
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
+	<?php 
+   $custom_logo_id = get_theme_mod( 'custom_logo' );
+   $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+      ?>
+<img src="<?php echo $image[0]; ?>" alt="">
 </header>
--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -67,8 +67,8 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>Clean Blog</h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1>Declans Website</h1>
+            <span class="subheading">Your heading</span>
           </div>
         </div>
       </div>
