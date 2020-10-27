b1759d9dcc9fff199b4d6dc0277d4645aae403a5
Declan200 <declan2317@hotmail.com>
2020-10-28 10:57:43 +1100

    Update page.php

--- page.php
+++ page.php
@@ -17,8 +17,15 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
+<!-- Home Page ACF Functionality -->
+<h2><?php the_field('home_page_header_text'); ?></h2>
+	<p><?php the_field('home_page_description_text'); ?><p>
+<!-- About Page ACF Functionality	 -->
+	<h2><?php the_field('about_page_header_text'); ?></h2>
+	<p><?php the_field('about_page_description_text'); ?><p>
+<!-- Contact Page ACF Functionality	 -->
+	<h2><?php the_field('contact_page_header_text'); ?></h2>
+	<p><?php the_field('contact_page_description_text'); ?><p>
 <?php comments_template( '', true ); ?>
 <?php endwhile; ?>
 
